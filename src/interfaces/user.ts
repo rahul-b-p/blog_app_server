3296f341f3b711e2452122b08494f92432f3a130
rahulbp17 <rahulzantoz10@gmail.com>
2025-10-30 20:46:07 +0530

    fix(user): resolve issues in mapping

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,11 +1,14 @@
-import { Document } from "mongoose";
+import { Document, ObjectId } from "mongoose";
 import { UserRole } from "../enums";
 
 export interface IUser extends Document{
+  _id:ObjectId
   fullName: string;
   username: string;
   email: string;
   passwordHash: string;
   role: UserRole;
   verified:boolean;
+  createdAt:Date;
+  updatedAt:Date;
 }
--- src/mapping/mapper.ts
+++ src/mapping/mapper.ts
@@ -1,6 +1,5 @@
 import { createMapper, Mapper } from '@automapper/core';
 import { classes } from '@automapper/classes';
-import { configureUserMapping } from './user.mapper';
 
 // Initialize the mapper with the classes strategy
 export const mapper: Mapper = createMapper({
